1db20c4b5286e31d1d48aee514e76a45657d3bb6
SagarR03 <sagarsagi360@gmail.com>
2025-02-03 23:51:18 +0530

    feat: update

--- variables.tf
+++ variables.tf
@@ -6,38 +6,38 @@ variable "resource_group_name" {
 variable "resource_group_description" {
   description = "The description of the AWS resource group"
   type        = string
-  default     = "Managed by Terraform"
+  default     = "Managed by Terraform - Updated"
 }
 
 variable "resource_query_resource_type_filter" {
   description = "The resource query as a JSON string"
   type        = string
-  default     = "AWS::EC2::Instance"
+  default     = "AWS::S3::Bucket"
 }
 
 variable "resource_query_tag_key" {
   description = "The key for the tag filter in the resource query"
   type        = string
-  default     = "Environment"
+  default     = "Project"
 }
 
 variable "resource_query_tag_values" {
   description = "The values for the tag filter in the resource query"
   type        = list(string)
-  default     = ["Test"]
+  default     = ["Development"]
 }
 
 variable "resource_query_type" {
   description = "The resource query as a JSON string"
   type        = string
-  default     = "TAG_FILTERS_1_0"
+  default     = "CLOUDFORMATION_STACK_1_0"
 }
 
 variable "resource_group_tags" {
   description = "Tags for the AWS resource group"
   type        = map(string)
   default     = {
-    Environment = "Test"
-    Project     = "personal-project"
+    Environment = "Development"
+    Project     = "updated-project"
   }
 }
